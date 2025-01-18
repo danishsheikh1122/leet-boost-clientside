2e7179d678fbddabfe568e79b667d1c71b95ce70
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2025-01-18 16:00:46 +0530

    added:-on click of toolbar icon sidepanel will open

--- src/background.js
+++ src/background.js
@@ -1,39 +1,4 @@
-// // whatever link will be used the extension is restricted to it self only and change it as u want
-// const GOOGLE_ORIGIN = 'https://www.leetcode.com';
-
-// // Enable the side panel on google.com and disable it on other sites
-// chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-//   if (!tab.url) return;
-//   const url = new URL(tab.url);
-
-//   // Enable the side panel only on google.com
-//   if (url.origin === GOOGLE_ORIGIN) {
-//     await chrome.sidePanel.setOptions({
-//       tabId,
-//       path: 'sidepanel.html',
-//       enabled: true
-//     });
-//   } else {
-//     // Disable the side panel on other sites
-//     await chrome.sidePanel.setOptions({
-//       tabId,
-//       enabled: false
-//     });
-//   }
-// });
-
-// // Allows users to open the side panel by clicking the toolbar icon
-// chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
-
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//     if (message.type === 'open_side_panel') {
-//       // Open the side panel using Chrome's sidePanel API
-//       chrome.sidePanel.open({
-//         path: 'sidepanel.html'  // Path to the side panel content
-//       });
-//     }
-//   });
-
+// this will open side bar only on leetcode.com
 const LEETCODE_ORIGIN = "https://leetcode.com"
 
 // Listen for tab updates to enable/disable the side panel
@@ -57,17 +22,8 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   }
 })
 
-// Allow users to open the side panel by clicking the extension icon
-chrome.action.onClicked.addListener((tab) => {
-  chrome.runtime.sendMessage({ type: "open_side_panel" })
-})
+// Allow users to open the side panel by clicking the extension icon MOST IMP
 
-// Handle runtime messages for opening the side panel
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "open_side_panel") {
-    chrome.sidePanel.setOptions({
-      path: "sidepanel.html", // Path to your side panel content
-      enabled: true
-    })
-  }
-})
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
